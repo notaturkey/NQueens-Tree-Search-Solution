55f309bcdebce4d756eee275d01b840f93c11c57
Thomas  McDonald <Thomas.Mcdonald@samtec.com>
2019-10-02 13:05:15 -0600

    almost done

--- main.py
+++ main.py
@@ -135,19 +135,6 @@ def expandNodes(arr,goal):
     right =  NPuzzle(movePOS(3,copy.deepcopy(arr)),goal)
     return [copy.deepcopy(up),copy.deepcopy(down),copy.deepcopy(left),copy.deepcopy(right)]
 
-
-goal = initNBoard(9)
-board = shuffleBoard(initNBoard(9))
-
-npuzzle = NPuzzle(board,goal)
-arr = npuzzle.getBoard()
-print("Starting array:\n"+str(arr))
-
-print("goal:\n"+str(npuzzle.getGoal())+"\n")
-queue = []
-states = []
-depth = 0
-queue.append(npuzzle)
 def graphSearch(search, queue, states):
     global depth
     if search == "bf":
@@ -188,73 +175,21 @@ def graphSearch(search, queue, states):
                             if i.getBoard() not in states:
                                 queue.append(i)
 
-
-
-
-
-    #     if puzzle.isGoal():
-    #         return puzzle
-    #     elif puzzle.depth < 300:
-    #         states.append(copy.deepcopy(puzzle.getBoard()))
-    #         explore = expandNodes(copy.deepcopy(puzzle.getBoard()),puzzle.getGoal())
-    #         depth = depth +1
-    #         for i in copy.deepcopy(explore): 
-    #             if not i.getBoard():
-    #                 continue
-    #             puzzle.child.append(i)
-    #             i.parent = puzzle
-    #             if copy.deepcopy(i.getBoard()) not in copy.deepcopy(states):
-    #                 i.depth = depth
-    #                 queue.append(copy.deepcopy(i))
-            
-    #         try:
-    #             x = queue.pop(0)
-    #         except:
-    #             x = False
-    #         if x:
-    #             return graphSearch(copy.deepcopy(x), "bf", queue,states)
-    #     else:
-    #         try:
-    #             x = queue.pop(0)
-    #         except:
-    #             x = False
-    #         if x:
-    #             return graphSearch(copy.deepcopy(x), "bf", queue,states)
-    # if search == "df":
-    #     if puzzle.isGoal():
-    #         return puzzle
-    #     elif puzzle.depth < 3:
-    #         states.append(copy.deepcopy(puzzle.getBoard()))
-    #         explore = expandNodes(copy.deepcopy(puzzle.getBoard()),puzzle.getGoal())
-    #         depth = depth +1
-    #         for i in copy.deepcopy(explore): 
-    #             if not i.getBoard():
-    #                 continue
-    #             puzzle.child.append(i)
-    #             i.parent = puzzle
-    #             if copy.deepcopy(i.getBoard()) not in copy.deepcopy(states):
-    #                 i.depth = depth
-    #                 queue.append(copy.deepcopy(i))
-    #                 try:
-    #                     x = queue.pop(0)
-    #                 except:
-    #                     x = False
-    #                 if x:
-    #                     graphSearch(copy.deepcopy(x), "df", queue,states)
-    #     else:
-    #         try:
-    #             x = queue.pop(0)
-    #         except:
-    #             x = False
-    #         if x:
-    #             graphSearch(copy.deepcopy(x), "df", queue,states)
-
+goal = initNBoard(9)
+board = shuffleBoard(initNBoard(9))
+npuzzle = NPuzzle(board,goal)
+arr = npuzzle.getBoard()
+print("Starting array:\n"+str(arr))
+print("goal:\n"+str(npuzzle.getGoal())+"\n")
+queue = []
+states = []
+depth = 0
+queue.append(npuzzle)
 result=""
 try:
     result = graphSearch("bf", queue, states)
 except:
     print("couldnt find solution") 
-
 print("Path is:")
 if result:           
     while True:
@@ -265,7 +200,6 @@ if result:
             break
 else:
     print("didnt find with maximum depth = 300")
-
 npuzzle = NPuzzle(board,goal)
 arr = npuzzle.getBoard()
 print("Starting array:\n"+str(arr))
@@ -275,8 +209,6 @@ states = []
 queue.append(npuzzle)
 # depth = 0
 result = graphSearch("df", queue, states)
-
-
 print("Path is:")
 if result:           
     while True:
@@ -288,8 +220,64 @@ if result:
 else:
     print("didnt find with maximum depth = 300")
 
-
-
 ###############################################
 # NQUENNS YAY
 ################################################
+
+class NQueen:
+    def __init__(self,board,goal):
+        self.board = board
+        self.goal = goal
+        self.child = []
+        self.parent = ""
+        self.depth = 0
+    def setBoard(self,board):
+        self.board = board
+    def setGoal(self,goal):
+        self.goal = goal
+    #def __init__(self, board, goal):
+    #    self.board = board
+    #    self.goal = goal
+    
+    def findMarks():
+        pass
+
+    def isGoal(self):
+        if self.board == self.goal:
+            return True
+        else:
+            return False
+
+    def getBoard(self):
+        return copy.deepcopy(self.board)
+    def getGoal(self):
+        return copy.deepcopy(self.goal)
+
+def initQBoard(n):
+    length = int(math.sqrt(n))
+    arr = [[0 for i in range(length)] for i in range(length)]
+    for i in arr:
+        i[0] = "X"
+    return arr
+
+board = initQBoard(9)
+print("queen board:\n"+str(board))
+def findQMark(arr):
+    column = ''
+    row = ''
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if arr[i][j] == "X":
+                column = j
+                row = i
+    return (row,column)
+
+def moveQ(col, direction):
+    
+
+def expandQNodes(arr,goal):
+    up = NPuzzle(movePOS(0,copy.deepcopy(arr)),goal)
+    down = NPuzzle(movePOS(1,copy.deepcopy(arr)),goal)
+    left = NPuzzle(movePOS(2,copy.deepcopy(arr)),goal)
+    right =  NPuzzle(movePOS(3,copy.deepcopy(arr)),goal)
+    return [copy.deepcopy(up),copy.deepcopy(down),copy.deepcopy(left),copy.deepcopy(right)]
